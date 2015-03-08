8060de268ed071ae5ef7dae71b2a7d9472805443
Sharon Connell <sharon.connell@careerbuilder.com>
2015-03-08 19:42:28 -0400

    roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 AjaxUrl::Application.routes.draw do
-  root 'links#public'
+
   resources :links
+  root 'links#index'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
